3b695df7a66f7d2e6b58fa6447b20f7d89a1142b
hp <hp@LAPTOP-14G6ACJ1>
2025-07-01 13:19:11 +0530

    Mama package added

---
+++ Selenium_1230/src/leeeemama/Mama420.java
@@ -0,0 +1,15 @@
+package leeeemama;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Mama420 {
+	@Test
+	public void launch()
+	{
+		WebDriver driver=new ChromeDriver();
+		Reporter.log("Deopavan 420",false);
+	}
+}
